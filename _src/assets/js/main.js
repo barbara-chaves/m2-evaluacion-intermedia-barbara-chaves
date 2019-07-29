2cc85826821208aaa7f75c370f45cc303edd6935
barbara-chaves <barbarabarcellos90@gmail.com>
2019-07-29 17:10:14 +0200

    improve script

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -4,26 +4,37 @@ const guess = document.querySelector("#user-number");
 const btn = document.querySelector(".btn");
 const outputText = document.querySelector(".text");
 const intentNumber = document.querySelector(".intent-number");
-let times = 0;
 
 const randonNumber = () => Math.ceil(Math.random() * 100);
 
 const randonNumberGenerate = randonNumber();
-console.log(randonNumberGenerate);
 
-const intentTimes = () => (intentNumber.innerHTML = times);
+const isValidNumber = number => {
+  if (number >= 0 && number <= 100) {
+    return true;
+  }
+};
 
-const helpGuess = () => {
-  if (randonNumberGenerate == guess.value) {
+const intentTimes = () =>
+  (intentNumber.innerHTML = parseInt(intentNumber.innerHTML) + 1);
+
+const messagerGenerate = () => {
+  if (randonNumberGenerate === parseInt(guess.value)) {
     outputText.innerHTML = "¡HAS GANADO, CAMPEONA!";
   } else if (randonNumberGenerate > guess.value) {
     outputText.innerHTML = "demasiado bajo";
   } else {
     outputText.innerHTML = "demasiado alto";
   }
-  times += 1;
-  intentTimes();
-  console.log(times);
+};
+
+const helpGuess = () => {
+  if (isValidNumber(guess.value)) {
+    messagerGenerate();
+    intentTimes();
+  } else {
+    outputText.innerHTML = "Escribe un número y dale a Prueba";
+  }
 };
 
 btn.addEventListener("click", helpGuess);
