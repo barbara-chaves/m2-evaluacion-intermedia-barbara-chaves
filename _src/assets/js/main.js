0da4dd1bb9b009a094f66dc9e1e53e58339bb8b3
barbara-chaves <barbarabarcellos90@gmail.com>
2019-07-29 15:48:58 +0200

    do script

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,23 @@
-'use strict';
+"use strict";
 
-console.log('>> Ready :)');
+const guess = document.querySelector("#user-number");
+const btn = document.querySelector(".btn");
+const outputText = document.querySelector(".text");
+const intentNumber = document.querySelector(".intent-number");
+
+const randonNumber = () => Math.ceil(Math.random() * 100);
+
+const randonNumberGenerate = randonNumber();
+console.log(randonNumberGenerate);
+
+const helpGuess = () => {
+  if (randonNumberGenerate == guess.value) {
+    outputText.innerHTML = "¡HAS GANADO, CAMPEONA!";
+  } else if (randonNumberGenerate > guess.value) {
+    outputText.innerHTML = "demasiado bajo";
+  } else {
+    outputText.innerHTML = "demasiado alto";
+  }
+};
+
+btn.addEventListener("click", helpGuess);
